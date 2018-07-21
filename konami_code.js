a49e365e7f3a7c205fceb5b17b90ad4d5d77b339
DevinPierce <devinmpierce@gmail.com>
2018-07-21 10:38:10 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,19 @@ const codes = [
   "a"
 ];
 
+const codeInput = []
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', checkCode)
+}
+
+function checkCode(event) {
+  codeInput.push(event.key)
+  if (codeInput[codeInput.length - 1] !== codes[codeInput.length - 1]){
+    codeInput.length = 0
+  }
+  if (codeInput.length === codes.length){
+    alert("KOJIMA IS GOD!")
+    codeInput.length = 0
+  }
 }
